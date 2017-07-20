7f5e610581b517352027e6143c580a9875b67973
James Schmidt <james.daniel.schmidt@gmail.com>
2017-07-20 13:34:03 -0600

    Update exercise-01-saying-hello/SayingHello.java

--- exercise-01-saying-hello/SayingHello.java
+++ exercise-01-saying-hello/SayingHello.java
@@ -1,3 +1,9 @@
+/**
+ * A program that prompts for your name and prints a greeting using your name.
+ * Exercise 1 Saying Hello, Exercises for Programmers by Brian Hogan
+ *
+ * @author James Schmidt
+*/
 class SayingHello {
 	public static void main(String args[]) {
 		String name = System.console().readLine("What is your name? ");
