88e79dffb1f5e6121875b06f871a1db0e91dd79d
James Schmidt <james.daniel.schmidt@gmail.com>
2017-07-20 18:48:37 -0600

    Add exercise-03-printing-quotes/PrintingQuotes.java

---
+++ exercise-03-printing-quotes/PrintingQuotes.java
@@ -0,0 +1,13 @@
+/**
+ * A program that prompts for quote and author and then displays the quotation.
+ * Exercise 3 Printing Quotes, Exercises for Programmers by Brian Hogan
+ *
+ * @author James Schmidt
+*/
+class PrintingQuotes {
+	public static void main(String[] args) {
+		String quote = System.console().readLine("What is the quote? ");
+		String author = System.console().readLine("Who said it? ");
+		System.out.println(author + " said \"" + quote + "\"");
+	}
+}
