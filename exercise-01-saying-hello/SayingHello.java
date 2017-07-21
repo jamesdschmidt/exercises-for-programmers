0b1815ea100ffaee8977f178fd33cb344c293eaa
James Schmidt <james.daniel.schmidt@gmail.com>
2017-07-20 18:28:42 -0600

    Add exercise-02-counting-characters/CountingCharacters.java

--- exercise-01-saying-hello/SayingHello.java
+++ exercise-01-saying-hello/SayingHello.java
@@ -5,7 +5,7 @@
  * @author James Schmidt
 */
 class SayingHello {
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		String name = System.console().readLine("What is your name? ");
 		String greeting = "Hello, " + name + " nice to meet you!";
 		System.out.println(greeting);
---
+++ exercise-02-counting-characters/CountingCharacters.java
@@ -0,0 +1,12 @@
+/**
+ * A program that prompts for an input string and displays the string and number of characters in the string.
+ * Exercise 2 Counting the Number of Characters, Exercises for Programmers by Brian Hogan
+ *
+ * @author James Schmidt
+*/
+class CountingCharacters {
+	public static void main(String[] args) {
+		String inputString = System.console().readLine("What is the input string? ");
+		System.out.format("%s has %d characters.%n", inputString, inputString.length());
+	}
+}
