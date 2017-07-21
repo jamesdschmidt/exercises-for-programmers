c7e8c8a8fdfd910d3af57c4a9caaaf908900cb49
James Schmidt <james.daniel.schmidt@gmail.com>
2017-07-20 18:54:08 -0600

    Add exercise 4

---
+++ exercise-04-mad-lib/MadLib.java
@@ -0,0 +1,16 @@
+/**
+ * A program that prompts for the parts of speech and then displays a constructed story.
+ * Exercise 4 Mad Lib, Exercises for Programmers by Brian Hogan
+ *
+ * @author James Schmidt
+*/
+class MadLib {
+	public static void main(String[] args) {
+		String noun = System.console().readLine("Enter a noun: ");
+		String verb = System.console().readLine("Enter a verb: ");
+		String adjective = System.console().readLine("Enter an adjective: ");
+		String adverb = System.console().readLine("Enter an adverb: ");
+
+		System.out.format("Do you %s your %s %s %s? That's hilarious!\n", verb, adjective, noun, adverb);
+	}
+}
