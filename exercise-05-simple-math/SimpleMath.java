33a223ea99d910ea774fa08ef04e15678aad7008
James Schmidt <james.daniel.schmidt@gmail.com>
2017-07-20 19:34:41 -0600

    Clean up whitespace in exercise 5

--- exercise-05-simple-math/SimpleMath.java
+++ exercise-05-simple-math/SimpleMath.java
@@ -8,15 +8,15 @@ class SimpleMath {
 	public static void main(String[] args) {
 		String input = System.console().readLine("What is the first number? ");
 		int firstNumber = Integer.parseInt(input);
-		
+
 		input = System.console().readLine("What is the second number? ");
 		int secondNumber = Integer.parseInt(input);
-		
+
 		int sum = firstNumber + secondNumber;
 		int difference = firstNumber - secondNumber;
 		int product = firstNumber * secondNumber;
 		int quotient = firstNumber / secondNumber;
-		
+
 		System.out.format("%1$d + %2$d = %3$d%n%1$d - %2$d = %4$d%n%1$d * %2$d = %5$d%n%1$d / %2$d = %6$d%n",
 			firstNumber, secondNumber, sum, difference, product, quotient);
 	}
