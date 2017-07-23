a9791b03e98a607e2a15f81cf2bc0d313d8fa4cc
James Schmidt <james.daniel.schmidt@gmail.com>
2017-07-23 00:13:41 -0600

    Add exercise 14

---
+++ exercise-14-tax-calculator/TaxCalculator.java
@@ -0,0 +1,29 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+/**
+ * A program to compute the tax on an order amount.
+ * Exercise 14 Tax Calculator, Exercises for Programmers by Brian Hogan
+ *
+ * @author James Schmidt
+*/
+class TaxCalculator {
+	public static void main(String[] args) {
+		final BigDecimal TAX_RATE = new BigDecimal(0.055);
+
+		String input = System.console().readLine("What is the order amount? ");
+		BigDecimal amount = new BigDecimal(input);
+
+		String state = System.console().readLine("What is the state? ");
+
+		BigDecimal total = amount;
+		String output = "";
+		if (state.equals("WI")) {
+			BigDecimal tax = amount.multiply(TAX_RATE);
+			output = String.format("The subtotal is $%s.%nThe tax is $%s%n", amount.setScale(2, RoundingMode.HALF_UP), tax.setScale(2, RoundingMode.HALF_UP));
+			total = total.add(tax);
+		}
+		output += String.format("The total is $%s", total.setScale(2, RoundingMode.HALF_UP));
+
+		System.out.println(output);
+	}
+}
