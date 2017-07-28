5b1a7fdaec1ffc2e972d5de409ce6abe8f7323bd
James Schmidt <james.daniel.schmidt@gmail.com>
2017-07-28 12:00:48 -0600

    Update exercise 21 to use a list

--- exercise-21-numbers-to-names/NumbersToNames.java
+++ exercise-21-numbers-to-names/NumbersToNames.java
@@ -1,5 +1,5 @@
-import java.math.BigDecimal;
-import java.math.RoundingMode;
+import java.util.Arrays;
+import java.util.List;
 /**
  * A program to convert a number from 1 to 12 to a month name.
  * Exercise 21 Numbers to Names, Exercises for Programmers by Brian Hogan
@@ -8,51 +8,13 @@ import java.math.RoundingMode;
 */
 class NumbersToNames {
 	public static void main(String[] args) {
-		String input = System.console().readLine("Please enter the number of the month: ");
-		int number = Integer.parseInt(input);
+		List<String> months = Arrays.asList("January", "February", "March", "April", "May", "June",
+			"July", "August", "September", "October", "November", "December");
 
-		String month = "";
-		switch (number) {
-			case 1:
-				month = "January";
-				break;
-			case 2:
-				month = "February";
-				break;
-			case 3:
-				month = "March";
-				break;
-			case 4:
-				month = "April";
-				break;
-			case 5:
-				month = "May";
-				break;
-			case 6:
-				month = "June";
-				break;
-			case 7:
-				month = "July";
-				break;
-			case 8:
-				month = "August";
-				break;
-			case 9:
-				month = "September";
-				break;
-			case 10:
-				month = "October";
-				break;
-			case 11:
-				month = "November";
-				break;
-			case 12:
-				month = "December";
-				break;
-			default:
-				month = "invalid";
-				break;
-		}
+		String input = System.console().readLine("Please enter the number of the month: ");
+		int index = Integer.parseInt(input) - 1;
+
+		String month = (index >= 0 && index <= 11) ? months.get(index) : "invalid";
 
 		System.out.format("The name of the month is %s.%n", month);
 	}
