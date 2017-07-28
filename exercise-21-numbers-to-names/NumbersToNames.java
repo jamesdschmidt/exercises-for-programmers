caa8bf3d629eee8559f82b12bea0fd082611f0be
James Schmidt <james.daniel.schmidt@gmail.com>
2017-07-28 12:22:44 -0600

    Update exercise 21 to use calendar class

--- exercise-21-numbers-to-names/NumbersToNames.java
+++ exercise-21-numbers-to-names/NumbersToNames.java
@@ -1,5 +1,5 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Calendar;
+import java.util.Locale;
 /**
  * A program to convert a number from 1 to 12 to a month name.
  * Exercise 21 Numbers to Names, Exercises for Programmers by Brian Hogan
@@ -8,24 +8,15 @@ import java.util.Map;
 */
 class NumbersToNames {
 	public static void main(String[] args) {
-		Map<Integer, String> months = new HashMap<>();
-		months.put(1, "January");
-		months.put(2, "February");
-		months.put(3, "March");
-		months.put(4, "April");
-		months.put(5, "May");
-		months.put(6, "June");
-		months.put(7, "July");
-		months.put(8, "August");
-		months.put(9, "September");
-		months.put(10, "October");
-		months.put(11, "November");
-		months.put(12, "December");
-
 		String input = System.console().readLine("Please enter the number of the month: ");
 		int number = Integer.parseInt(input);
 
-		String month = (number >= 1 && number <= 12) ? months.get(number) : "invalid";
+		String month = "invalid";
+		if (number >= 1 && number <= 12) {
+			Calendar calendar = Calendar.getInstance();
+			calendar.set(Calendar.MONTH, number - 1);
+			month = calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.ENGLISH);
+		}
 
 		System.out.format("The name of the month is %s.%n", month);
 	}
