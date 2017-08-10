5bdecdd91f41bb4148eac96df6e2f013b02e9a33
James Schmidt <james.daniel.schmidt@gmail.com>
2017-08-10 17:29:55 -0600

    Add exercise 30

---
+++ chapter-06/exercise-30-multiplication-table/MultiplicationTable.java
@@ -0,0 +1,32 @@
+/**
+ * A program that generates multiplication tables for the numbers 0 through 12.
+ * Exercise 30 Multiplication Table, Exercises for Programmers by Brian Hogan
+ *
+ * @author James Schmidt
+*/
+class MultiplicationTable {
+	public static void main(String[] args) {
+		final int MAX = 12;
+		final int COLUMN_WIDTH = 4;
+		final String FORMAT = "%" + COLUMN_WIDTH + "d";
+
+		// Print header row
+		System.out.printf("%" + COLUMN_WIDTH + "s", "");
+		for (int i = 0; i <= MAX; i++) {
+			System.out.printf(FORMAT, i);
+		}
+		System.out.println();
+
+		// Print rows
+		for (int i = 0; i <= MAX; i++) {
+			// Print row header
+			System.out.printf(FORMAT, i);
+
+			// Print row
+			for (int j = 0; j <= MAX; j++) {
+				System.out.printf(FORMAT, i * j);
+			}
+			System.out.println();
+		}
+	}
+}
