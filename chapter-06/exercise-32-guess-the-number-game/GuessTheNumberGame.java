04ac8332351a1fd3c4c0390827d9865e93f243b1
James Schmidt <james.daniel.schmidt@gmail.com>
2017-08-10 21:42:06 -0600

    Add exercise 32

---
+++ chapter-06/exercise-32-guess-the-number-game/GuessTheNumberGame.java
@@ -0,0 +1,72 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * A guess the number game.
+ * Exercise 32 Guess the Number Game, Exercises for Programmers by Brian Hogan
+ *
+ * @author James Schmidt
+*/
+class GuessTheNumberGame {
+	public static void main(String[] args) {
+		System.out.println("Let's play Guess the Number.");
+
+		while (true) {
+			playGame();
+			if (!playAgain()) {
+				break;
+			}
+		}
+
+		System.out.println("Goodbye!");
+	}
+
+	private static int getDifficulty() {
+		String input;
+		do {
+			input = System.console().readLine("Pick a difficulty level (1, 2, or 3): ");
+		} while (!input.matches("1|2|3"));
+		return Integer.parseInt(input);
+	}
+
+	private static int getGuess(String message) {
+		int value;
+		String input = System.console().readLine(message);
+		try {
+			value = Integer.parseInt(input);
+		} catch (NumberFormatException e) {
+			value = -1;
+		}
+		return value;
+	}
+
+	private static int getRandomNumber(int difficulty) {
+		return ThreadLocalRandom.current().nextInt(1, (int) Math.pow(10, difficulty) + 1);
+	}
+
+	private static boolean playAgain() {
+		String input = System.console().readLine("Play again? ");
+		return input.toLowerCase().startsWith("y");
+	}
+
+	private static void playGame() {
+		int tries = 1;
+		int difficulty = getDifficulty();
+		int number = getRandomNumber(difficulty);
+
+		int guess = getGuess("I have my number. What's your guess? ");
+		while (guess != number) {
+			tries++;
+			String prompt;
+			if (guess == -1) {
+				prompt = "Wrong guess. Guess again: ";
+			} else if (guess < number) {
+				prompt = "Too low. Guess again: ";
+			} else {
+				prompt = "Too high. Guess again: ";
+			}
+			guess = getGuess(prompt);
+		}
+
+		System.out.printf("You got it in %d guesses!%n", tries);
+	}
+}
