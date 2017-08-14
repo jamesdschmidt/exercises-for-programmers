e3aa0d979918a63609a8ef788177f939df5acb04
James Schmidt <james.daniel.schmidt@gmail.com>
2017-08-14 12:58:46 -0600

    Add exercise 36

---
+++ chapter-07/exercise-36-computing-statistics/ComputingStatistics.java
@@ -0,0 +1,94 @@
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A program that prompts for response times and computes statistics.
+ * Exercise 36 Computing Statistics, Exercises for Programmers by Brian Hogan
+ *
+ * @author James Schmidt
+*/
+class ComputingStatistics {
+	public static void main(String[] args) {
+		List<Long> responseTimes = new ArrayList<>();
+
+		while (true) {
+			String input = System.console().readLine("Enter a number: ");
+			if ("done".equals(input)) {
+				break;
+			} else if (isNumeric(input)) {
+				responseTimes.add(Long.parseLong(input));
+			}
+		}
+
+		if (responseTimes.size() > 0) {
+			long average = calculateAverage(responseTimes);
+			long minimum = calculateMinimum(responseTimes);
+			long maximum = calculateMaximum(responseTimes);
+			double standardDeviation = calculateStandardDeviation(responseTimes);
+
+			printResponseTimes(responseTimes);
+			System.out.printf("The average is %d.%n", average);
+			System.out.printf("The minimum is %d.%n", minimum);
+			System.out.printf("The maximum is %d.%n", maximum);
+			System.out.printf("The standard deviation is %.2f.%n", standardDeviation);
+		}
+	}
+
+	private static long calculateAverage(List<Long> list) {
+		long sum = 0;
+		for (long value : list) {
+			sum += value;
+		}
+		return sum / list.size();
+	}
+
+	private static long calculateMinimum(List<Long> list) {
+		long minimum = list.get(0);
+		for (long value : list) {
+			minimum = minimum > value ? value : minimum;
+		}
+		return minimum;
+	}
+
+	private static long calculateMaximum(List<Long> list) {
+		long maximum = list.get(0);
+		for (long value : list) {
+			maximum = maximum < value ? value : maximum;
+		}
+		return maximum;
+	}
+
+	private static double calculateStandardDeviation(List<Long> list) {
+		long mean = calculateAverage(list);
+
+		double sum = 0;
+		for (long value : list) {
+			sum += Math.pow(value - mean, 2);
+		}
+
+		double squaredMean = sum / list.size();
+		return Math.sqrt(squaredMean);
+	}
+
+	private static boolean isNumeric(String s) {
+		if (s == null || s.isEmpty()) {
+			return false;
+		}
+		for (char c : s.toCharArray()) {
+			if (!Character.isDigit(c)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static void printResponseTimes(List<Long> responseTimes) {
+		System.out.print("Numbers: ");
+		for (int i = 0; i < responseTimes.size(); i++) {
+			System.out.print(responseTimes.get(i) + (i < responseTimes.size() - 1 ? ", " : ""));
+		}
+		System.out.println();
+	}
+}
