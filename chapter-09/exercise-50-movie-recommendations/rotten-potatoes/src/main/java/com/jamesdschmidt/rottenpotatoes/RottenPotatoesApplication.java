eea00301496457e7c2771982119119e0cd6140ea
James Schmidt <james.daniel.schmidt@gmail.com>
2017-09-11 07:42:45 -0600

    Start exercise 50

---
+++ chapter-09/exercise-50-movie-recommendations/rotten-potatoes/src/main/java/com/jamesdschmidt/rottenpotatoes/RottenPotatoesApplication.java
@@ -0,0 +1,12 @@
+package com.jamesdschmidt.rottenpotatoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RottenPotatoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RottenPotatoesApplication.class, args);
+	}
+}
