1c12a74a6878ad2e934b0442afa9ec36dba59a1c
James D. Schmidt <james.daniel.schmidt@gmail.com>
2018-03-28 21:00:20 -0600

    Add saying_hello.py

--- chapter-02/exercise-01-saying-hello/java/SayingHello.java
+++ chapter-02/exercise-01-saying-hello/java/SayingHello.java
@@ -7,7 +7,7 @@
 class SayingHello {
   public static void main(String[] args) {
     String name = System.console().readLine("What is your name? ");
-    String greeting = "Hello, " + name + " nice to meet you!";
+    String greeting = "Hello, " + name + ", nice to meet you!";
     System.out.println(greeting);
   }
 }
---
+++ chapter-02/exercise-01-saying-hello/python/saying_hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# A program that prompts for your name and prints a greeting using your name.
+# Exercise 1 Saying Hello, Exercises for Programmers by Brian Hogan
+#
+# by James Schmidt
+
+name = input("What is your name? ")
+greeting = "Hello, " + name + ", nice to meet you!"
+print(greeting)
