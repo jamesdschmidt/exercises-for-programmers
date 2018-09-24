7d9c85d81a74e00117684afe37f5e81e3f1d9761
James D. Schmidt <james.daniel.schmidt@gmail.com>
2018-09-24 08:34:44 -0600

    Add time client in exercise 52

---
+++ chapter-09/exercise-52-creating-your-own-time-service/time-client/src/main/java/com/example/timeclient/TimeClientApplication.java
@@ -0,0 +1,12 @@
+package com.example.timeclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeClientApplication.class, args);
+	}
+}
