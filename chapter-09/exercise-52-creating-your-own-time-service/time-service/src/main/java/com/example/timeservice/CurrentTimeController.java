5632aad67d4efc5bbb626c2dc75bbee1b4c1afef
James D. Schmidt <james.daniel.schmidt@gmail.com>
2018-09-24 08:23:16 -0600

    Switch to UTC

--- chapter-09/exercise-52-creating-your-own-time-service/time-service/src/main/java/com/example/timeservice/CurrentTimeController.java
+++ chapter-09/exercise-52-creating-your-own-time-service/time-service/src/main/java/com/example/timeservice/CurrentTimeController.java
@@ -3,7 +3,7 @@ package com.example.timeservice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 import java.util.Collections;
 import java.util.Map;
 
@@ -12,6 +12,6 @@ public class CurrentTimeController {
 
   @GetMapping
   public Map index() {
-    return Collections.singletonMap("currentTime", LocalDateTime.now());
+    return Collections.singletonMap("currentTime", Instant.now());
   }
 }
