bba6ef5c9108bc5be18260e53c47c6be374e81ce
James Schmidt <james.daniel.schmidt@gmail.com>
2018-09-25 23:03:48 -0600

    Update exercise 5

--- chapter-02/exercise-05-simple-math/SimpleMath.java
+++ chapter-02/exercise-05-simple-math/SimpleMath.java
@@ -6,18 +6,23 @@
 */
 class SimpleMath {
   public static void main(String[] args) {
-    String input = System.console().readLine("What is the first number? ");
-    int firstNumber = Integer.parseInt(input);
+    var input = gets("What is the first number? ");
+    var firstNumber = toInt(input);
 
-    input = System.console().readLine("What is the second number? ");
-    int secondNumber = Integer.parseInt(input);
+    input = gets("What is the second number? ");
+    var secondNumber = toInt(input);
 
-    int sum = firstNumber + secondNumber;
-    int difference = firstNumber - secondNumber;
-    int product = firstNumber * secondNumber;
-    int quotient = firstNumber / secondNumber;
+    var sum = firstNumber + secondNumber;
+    var difference = firstNumber - secondNumber;
+    var product = firstNumber * secondNumber;
+    var quotient = firstNumber / secondNumber;
 
-    System.out.printf("%1$d + %2$d = %3$d%n%1$d - %2$d = %4$d%n%1$d * %2$d = %5$d%n%1$d / %2$d = %6$d%n",
+    puts("%1$d + %2$d = %3$d%n%1$d - %2$d = %4$d%n%1$d * %2$d = %5$d%n%1$d / %2$d = %6$d%n",
       firstNumber, secondNumber, sum, difference, product, quotient);
   }
+
+  private static int toInt(String s) { return Integer.parseInt(s); }
+  private static String gets(String s) { return System.console().readLine(s); }
+  private static void puts(String format, Object... args) { System.out.printf(format, args); }
 }
+
