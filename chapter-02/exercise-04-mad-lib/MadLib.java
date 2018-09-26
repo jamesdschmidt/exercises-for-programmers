428bf4a02477c5b218314f66d672407e74caaa2b
James Schmidt <james.daniel.schmidt@gmail.com>
2018-09-25 22:47:53 -0600

    Update exercise 4

--- chapter-02/exercise-04-mad-lib/MadLib.java
+++ chapter-02/exercise-04-mad-lib/MadLib.java
@@ -6,11 +6,14 @@
 */
 class MadLib {
   public static void main(String[] args) {
-    String noun = System.console().readLine("Enter a noun: ");
-    String verb = System.console().readLine("Enter a verb: ");
-    String adjective = System.console().readLine("Enter an adjective: ");
-    String adverb = System.console().readLine("Enter an adverb: ");
+    var noun = gets("Enter a noun: ");
+    var verb = gets("Enter a verb: ");
+    var adjective = gets("Enter an adjective: ");
+    var adverb = gets("Enter an adverb: ");
 
-    System.out.printf("Do you %s your %s %s %s? That's hilarious!\n", verb, adjective, noun, adverb);
+    puts("Do you %s your %s %s %s? That's hilarious!%n", verb, adjective, noun, adverb);
   }
+
+  private static String gets(String s) { return System.console().readLine(s); }
+  private static void puts(String format, Object... args) { System.out.printf(format, args); }
 }
