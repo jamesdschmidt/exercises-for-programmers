ff1f0ed709aed81f32e67ecb1fb51df860184fdd
james <james@jamess-MacBook-Air.local>
2018-09-25 22:22:19 -0600

    Update exercise 1 for Java 11

--- chapter-02/exercise-01-saying-hello/SayingHello.java
+++ chapter-02/exercise-01-saying-hello/SayingHello.java
@@ -6,8 +6,8 @@
 */
 public class SayingHello {
   public static void main(String[] args) {
-    String name = System.console().readLine("What is your name? ");
-    String greeting = "Hello, " + name + ", nice to meet you!";
+    var name = System.console().readLine("What is your name? ");
+    var greeting = "Hello, " + name + ", nice to meet you!";
     System.out.println(greeting);
   }
 }
