9e11442433ab59652ba4b62979521ae326408114
James Schmidt <james.daniel.schmidt@gmail.com>
2018-09-27 21:13:08 -0600

    Update exercise 15

--- chapter-04/exercise-15-password-validation/PasswordValidation.java
+++ chapter-04/exercise-15-password-validation/PasswordValidation.java
@@ -1,17 +1,11 @@
-/**
- * A program that validates user login credentials.
- * Exercise 15 Password Validation, Exercises for Programmers by Brian Hogan
- *
- * @author James Schmidt
-*/
 class PasswordValidation {
+  private static final String REQUIRED_USERNAME = "username";
+  private static final String REQUIRED_PASSWORD = "abc$123";
+  
   public static void main(String[] args) {
-    final String REQUIRED_USERNAME = "username";
-    final String REQUIRED_PASSWORD = "abc$123";
-
-    String username = System.console().readLine("What is the username? ");
-    char[] passwordBuffer = System.console().readPassword("What is the password? ");
-    String password = new String(passwordBuffer);
+    var username = System.console().readLine("What is the username? ");
+    var passwordBuffer = System.console().readPassword("What is the password? ");
+    var password = new String(passwordBuffer);
 
     if (!REQUIRED_USERNAME.equals(username) || !REQUIRED_PASSWORD.equals(password)) {
       System.out.println("I don't know you.");
