b422c3e7e13c9777299a8ed58487785dfc73ddd2
James Schmidt <james.daniel.schmidt@gmail.com>
2018-09-27 21:23:33 -0600

    Update exercise 17

--- chapter-04/exercise-17-blood-alcohol-calculator/BloodAlcoholCalculator.java
+++ chapter-04/exercise-17-blood-alcohol-calculator/BloodAlcoholCalculator.java
@@ -1,50 +1,40 @@
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-/**
- * A program that calculates blood alcohol level.
- * Exercise 17 Blood Alcohol Calculator, Exercises for Programmers by Brian Hogan
- *
- * @author James Schmidt
-*/
 class BloodAlcoholCalculator {
+  private static final BigDecimal ALCOHOL_DISTRIBUTION_RATIO_MEN = new BigDecimal(0.73);
+  private static final BigDecimal ALCOHOL_DISTRIBUTION_RATIO_WOMEN = new BigDecimal(0.66);
+  private static final BigDecimal HOURS_MULTIPLIER = new BigDecimal(0.015);
+  private static final BigDecimal MAX_BLOOD_ALCOHOL_CONTENT = new BigDecimal(0.08);
+  private static final BigDecimal VOLUME_MULTIPLIER = new BigDecimal(5.14);
+  
   public static void main(String[] args) {
-    final BigDecimal ALCOHOL_DISTRIBUTION_RATIO_MEN = new BigDecimal(0.73);
-    final BigDecimal ALCOHOL_DISTRIBUTION_RATIO_WOMEN = new BigDecimal(0.66);
-    final BigDecimal HOURS_MULTIPLIER = new BigDecimal(0.015);
-    final BigDecimal MAX_BLOOD_ALCOHOL_CONTENT = new BigDecimal(0.08);
-    final BigDecimal VOLUME_MULTIPLIER = new BigDecimal(5.14);
+    var weight = getd("What is your weight? ");
 
-    BigDecimal weight = getBigDecimal("What is your weight? ");
+    var line = System.console().readLine("What is your gender? ");
+    var isMale = line.toLowerCase().startsWith("m");
+    var alcoholDistributionRatio = (isMale ? ALCOHOL_DISTRIBUTION_RATIO_MEN : ALCOHOL_DISTRIBUTION_RATIO_WOMEN);
 
-    String input = System.console().readLine("What is your gender? ");
-    boolean isMale = input.toLowerCase().startsWith("m");
-    BigDecimal alcoholDistributionRatio = (isMale ? ALCOHOL_DISTRIBUTION_RATIO_MEN : ALCOHOL_DISTRIBUTION_RATIO_WOMEN);
+    var ounces = getd("How many ounces of alcohol did you drink? ");
+    var hours = getd("What is the number of hours since last drink? ");
 
-    BigDecimal ounces = getBigDecimal("How many ounces of alcohol did you drink? ");
-    BigDecimal hours = getBigDecimal("What is the number of hours since last drink? ");
-
-    BigDecimal volumeOperand = ounces.multiply(VOLUME_MULTIPLIER);
-    BigDecimal weightOperand = weight.multiply(alcoholDistributionRatio);
-    BigDecimal hoursOperand = hours.multiply(HOURS_MULTIPLIER);
-    BigDecimal leftOperand = volumeOperand.divide(weightOperand, RoundingMode.HALF_UP);
-    BigDecimal bloodAlcoholContent = leftOperand.subtract(hoursOperand);
+    var volumeOperand = ounces.multiply(VOLUME_MULTIPLIER);
+    var weightOperand = weight.multiply(alcoholDistributionRatio);
+    var hoursOperand = hours.multiply(HOURS_MULTIPLIER);
+    var leftOperand = volumeOperand.divide(weightOperand, RoundingMode.HALF_UP);
+    var bloodAlcoholContent = leftOperand.subtract(hoursOperand);
 
     System.out.printf("Your BAC is %s%n", bloodAlcoholContent.setScale(3, RoundingMode.HALF_UP));
     System.out.println("It is " + (bloodAlcoholContent.compareTo(MAX_BLOOD_ALCOHOL_CONTENT) < 1 ? "" : "not ") + "legal for you to drive.");
   }
 
-  private static BigDecimal getBigDecimal(String prompt) {
-    BigDecimal value;
-    String line = System.console().readLine(prompt);
-
+  private static BigDecimal getd(String prompt) {
+    var line = System.console().readLine(prompt);
     try {
-      value = new BigDecimal(line);
+      return new BigDecimal(line);
     } catch (NumberFormatException e) {
       System.out.println("Enter valid number.");
-      value = getBigDecimal(prompt);
+      return getd(prompt);
     }
-
-    return value;
   }
 }
