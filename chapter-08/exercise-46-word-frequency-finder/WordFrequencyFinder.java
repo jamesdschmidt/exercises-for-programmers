e04ef8460341c31454515e445a888622dfe7e523
James Schmidt <james.daniel.schmidt@gmail.com>
2018-09-29 14:16:45 -0600

    Update exercise 46

--- chapter-08/exercise-46-word-frequency-finder/WordFrequencyFinder.java
+++ chapter-08/exercise-46-word-frequency-finder/WordFrequencyFinder.java
@@ -1,59 +1,45 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-/**
- * A program that counts the frequency of words read from a file.
- * Exercise 46 Word Frequency Finder, Exercises for Programmers by Brian Hogan
- *
- * @author James Schmid
- */
 public class WordFrequencyFinder {
   public static void main(String[] args) {
-    String words = readFile("words.txt");
-    System.out.println("words.txt:");
-    System.out.println(words);
+    var words = readFile("words.txt");
+    System.out.println("words.txt: " + words);
 
-    Map<String, Long> map = getWordFrequencies(words);
-    int width = getWidthOfLongestWord(map);
-    String format = "%-" + (width + 1) + "s ";
-    for (Entry<String, Long> pair : map.entrySet()) {
-      System.out.printf(format, pair.getKey() + ":");
-      System.out.println(Strings.repeat("*", pair.getValue().intValue()));
-    }
+    var map = getWordFrequencies(words);
+    var format = "%-" + (getWidthOfLongestWord(map) + 1) + "s ";
+    map.forEach((k, v) -> {
+      System.out.printf(format, k + ":");
+      System.out.println("*".repeat(v.intValue()));
+    });
   }
 
   private static String readFile(String fileName) {
-    String contents = "";
-
     try {
-      contents = new String(Files.readAllBytes(Paths.get(fileName)));
+      return new String(Files.readAllBytes(Paths.get(fileName)));
     } catch (IOException e) {
       e.printStackTrace();
     }
-
-    return contents;
+    return "";
   }
 
-  private static int getWidthOfLongestWord(Map<String, Long> map) {
-    int width = 0;
-    for (String key : map.keySet()) {
-      width = (key.length() > width) ? key.length() : width;
-    }
-    return width;
+  private static int getWidthOfLongestWord(Map<String, Long> m) {
+    return Collections.max(m.keySet(), Comparator.comparing(String::length)).length();
   }
 
-  private static Map<String, Long> getWordFrequencies(String text) {
-    Stream<String> stream = Stream.of(text.toLowerCase().split("\\W+")).parallel();
-    Map<String, Long> map = stream.collect(Collectors.groupingBy(String::toString, Collectors.counting()));
-    return map.entrySet()
-      .stream()
+  private static Map<String, Long> getWordFrequencies(String s) {
+    return Stream.of(s.toLowerCase().split("\\W+"))
+      .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+      .entrySet().stream()
       .sorted(Entry.comparingByValue(Comparator.reverseOrder()))
       .collect(Collectors.toMap(Entry::getKey, Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
   }
