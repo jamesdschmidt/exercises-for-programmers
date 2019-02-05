e2eb810617f23672204541e852e3160861ac9d59
James Schmidt <james.daniel.schmidt@gmail.com>
2019-02-04 19:42:14 -0700

    Add exercise 7 in python

---
+++ exercise-07-area-of-a-rectangular-room/rectangular_room.py
@@ -0,0 +1,10 @@
+FEET_TO_METERS = 0.09290304
+
+length = int(input("What is the length of the room in feet? "))
+width = int(input("What is the width of the room in feet? "))
+
+area = length * width
+area_in_meters = area * FEET_TO_METERS
+
+print(f"You entered dimensions of {length} feet by {width} feet.")
+print(f"The area is\n{area} square feet\n{area_in_meters} square meters")
