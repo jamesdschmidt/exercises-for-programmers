17a929424f7614b13fa6d1918538d5c5836dac06
James Schmidt <james.daniel.schmidt@gmail.com>
2019-02-06 12:12:25 -0700

    Add exercise 18 in python

---
+++ exercise-18-temperature-converter/temperature_converter.py
@@ -0,0 +1,10 @@
+to_celsius = input("Press C to convert from Fahrenheit to Celsius.\n"
+                   "Press F to convert from Celsius to Fahrenheit.\n"
+                   "Your choice: ").lower() == "c"
+temperature = float(input(f"Please enter the temperature in {'Fahrenheit' if to_celsius else 'Celsius'}: "))
+if to_celsius:
+    converted_temperature = (temperature - 32) * (5 / 9.0)
+else:
+    converted_temperature = (temperature * (9 / 5.0)) + 32
+print(f"The temperature in {'Celsius' if to_celsius else 'Fahrenheit'} is {converted_temperature:.1f}.")
+
