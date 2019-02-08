dc865b19de233e9d6fe4e9c7f29f3d5f39d38bd7
James Schmidt <james.daniel.schmidt@gmail.com>
2019-02-08 12:51:17 -0700

    Update exercise 24

--- exercise-24-anagram-checker/anagram_checker.py
+++ exercise-24-anagram-checker/anagram_checker.py
@@ -1,12 +1,15 @@
+def get_strings():
+    print("Enter two strings and I'll tell you if they are anagrams:")
+    return (input("Enter the first string: "),
+            input("Enter the second string: "))
+
 def is_anagram(first, second):
     if first and second and len(first) > 0 and len(first) == len(second):
         if sorted(first.lower()) == sorted(second.lower()):
             return True
     return False
 
-print("Enter two strings and I'll tell you if they are anagrams:")
-first = input("Enter the first string: ")
-second = input("Enter the second string: ")
+first, second = get_strings()
 is_anagram = is_anagram(first, second)
 print(f'"{first}" and "{second}" are ' \
       f'{"anargrams" if is_anagram else "not anagrams"}.')
