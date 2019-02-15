fabc469ae1b291cad56973d45299974706df086b
James Schmidt <james.daniel.schmidt@gmail.com>
2019-02-14 18:50:54 -0700

    Update exercise 33 in Java

--- exercise-33-magic-8-ball/Magic8Ball.java
+++ exercise-33-magic-8-ball/Magic8Ball.java
@@ -3,12 +3,9 @@ import java.util.concurrent.ThreadLocalRandom;
 class Magic8Ball {
   public static void main(String[] args) {
     String[] answers = {"Yes", "No", "Maybe", "Ask again later"};
-
     System.console().readLine("What's your question? ");
-
-    var index = ThreadLocalRandom.current().nextInt(0, answers.length + 1);
-    var answer = answers[index];
-
-    System.out.println(answer);
+    var index = ThreadLocalRandom.current().nextInt(0, answers.length);
+    System.out.println(answers[index]);
   }
 }
+
