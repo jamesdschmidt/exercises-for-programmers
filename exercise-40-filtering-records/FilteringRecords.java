2b91132708dd138e4c33bc902fa10b68da888aba
James Schmidt <james.daniel.schmidt@gmail.com>
2019-02-18 14:40:20 -0700

    Clean up methods

--- exercise-40-filtering-records/FilteringRecords.java
+++ exercise-40-filtering-records/FilteringRecords.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -8,43 +9,40 @@ class FilteringRecords {
   public static void main(String[] args) {
     var search = System.console().readLine("Enter a search string: ");
     var employees = getEmployees();
-    var results = searchEmployees(employees, search);
-    results = sortEmployees(results, "lastName");
-    printEmployees(results);
+    var results = search(employees, search);
+    results = sort(results, "last");
+    print(results);
   }
 
-  private static Map<String, String> getEmployee(String firstName, String lastName, String position, String separationDate) {
-    var employee = new HashMap<String, String>();
-    employee.put("firstName", firstName);
-    employee.put("lastName", lastName);
-    employee.put("position", position);
-    employee.put("separationDate", separationDate);
-    return employee;
+  private static Map<String, String> of(String first, String last, String position, String separationDate) {
+    return Map.of("first", first,
+                  "last", last,
+                  "position", position,
+                  "separationDate", separationDate);
   }
 
   private static List<Map<String, String>> getEmployees() {
-    var employees = new ArrayList<Map<String, String>>();
-    employees.add(getEmployee("John", "Johnson", "Manager", "2016-12-31"));
-    employees.add(getEmployee("Tou", "Xiong", "Software Engineer", "2016-10-05"));
-    employees.add(getEmployee("Michaela", "Michaelson", "District Manager", "2015-12-19"));
-    employees.add(getEmployee("Jake", "Jacobson", "Programmer", ""));
-    employees.add(getEmployee("Jacquelyn", "Jackson", "DBA", ""));
-    employees.add(getEmployee("Sally", "Weber", "Web Developer", "2015-12-18"));
-    return employees;
+    return Arrays.asList(
+        of("John", "Johnson", "Manager", "2016-12-31"),
+        of("Tou", "Xiong", "Software Engineer", "2016-10-05"),
+        of("Michaela", "Michaelson", "District Manager", "2015-12-19"),
+        of("Jake", "Jacobson", "Programmer", ""),
+        of("Jacquelyn", "Jackson", "DBA", ""),
+        of("Sally", "Weber", "Web Developer", "2015-12-18"));
   }
 
-  private static void printEmployees(List<Map<String, String>> employees) {
+  private static void print(List<Map<String, String>> employees) {
     System.out.println("Name                | Position          | Separation Date");
     System.out.println("--------------------|-------------------|----------------");
     employees.forEach(employee -> {
       System.out.printf("%-20s| %-18s| %s%n",
-        employee.get("firstName") + " " + employee.get("lastName"),
+        employee.get("first") + " " + employee.get("last"),
         employee.get("position"),
         employee.get("separationDate"));
     });
   }
 
-  private static List<Map<String, String>> searchEmployees(List<Map<String, String>> employees, String search) {
+  private static List<Map<String, String>> search(List<Map<String, String>> employees, String search) {
     var results = new ArrayList<Map<String, String>>();
     for (var i = 0; i < employees.size(); i++) {
       var employee = employees.get(i);
@@ -58,9 +56,10 @@ class FilteringRecords {
     return results;
   }
 
-  private static List<Map<String, String>> sortEmployees(List<Map<String, String>> employees, String field) {
+  private static List<Map<String, String>> sort(List<Map<String, String>> employees, String field) {
     Comparator<Map<String, String>> comparator = (Map<String, String> m1, Map<String, String> m2) -> m1.get(field).compareTo(m2.get(field));
     employees.sort(comparator);
     return employees;
   }
 }
+
