757c81a5d8207c089701ea07c4092125a1b0317e
James Schmidt <james.daniel.schmidt@gmail.com>
2019-02-18 14:05:46 -0700

    Add exercise 39 in python

---
+++ exercise-39-sorting-records/sorting_records.py
@@ -0,0 +1,25 @@
+from operator import itemgetter
+
+def employee(first, last, position, separation_date):
+    return {"first": first,
+            "last": last,
+            "position": position,
+            "separation_date": separation_date}
+
+def employees():
+    employees = [employee("John", "Johnson", "Manager", "2016-12-31"),
+                 employee("Tou", "Xiong", "Software Engineer", "2016-10-05"),
+                 employee("Michaela", "Michaelson", "District Manager",
+                          "2015-12-19"),
+                 employee("Jake", "Jacobson", "Programmer", ""),
+                 employee("Jacquelyn", "Jackson", "DBA", ""),
+                 employee("Sally", "Weber", "Web Developer", "2015-12-18")]
+    employees.sort(key = itemgetter("last"))
+    return employees
+
+print("Name                | Position          | Separation Date")
+print("--------------------|-------------------|----------------")
+for e in employees():
+    print(f"{e['first'] + ' ' + e['last']:<20}| {e['position']:<18}| " \
+          f"{e['separation_date']}")
+
