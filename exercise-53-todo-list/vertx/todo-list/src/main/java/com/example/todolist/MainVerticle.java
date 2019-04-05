f7902fc70719ecc44017ac9f4a6e60a8272a5e1e
James Schmidt <james.daniel.schmidt@gmail.com>
2019-04-04 22:15:28 -0600

    Add index

--- exercise-53-todo-list/vertx/todo-list/src/main/java/com/example/todolist/MainVerticle.java
+++ exercise-53-todo-list/vertx/todo-list/src/main/java/com/example/todolist/MainVerticle.java
@@ -2,23 +2,77 @@ package com.example.todolist;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.templ.thymeleaf.ThymeleafTemplateEngine;
+import io.vertx.redis.RedisOptions;
 
 public class MainVerticle extends AbstractVerticle {
+  private static final Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
+
+  private ThymeleafTemplateEngine templateEngine;
 
   @Override
   public void start(Future<Void> startFuture) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888, http -> {
-      if (http.succeeded()) {
+    Future<Void> steps = prepareDatabase().compose(v -> startHttpServer());
+    steps.setHandler(asyncResult -> {
+      if (asyncResult.succeeded()) {
         startFuture.complete();
-        System.out.println("HTTP server started on port 8888");
       } else {
-        startFuture.fail(http.cause());
+        startFuture.fail(asyncResult.cause());
       }
     });
+  }
+
+  private Future<Void> prepareDatabase() {
+    Future<Void> future = Future.future();
+    future.complete();
+    return future;
+  }
+
+  private Future<Void> startHttpServer() {
+    Future<Void> future = Future.future();
+    var server = vertx.createHttpServer();
+
+    var router = Router.router(vertx);
+    router.delete("/todos/:id").handler(this::delete);
+    router.get("/todos").handler(this::index);
+    router.post("/todos").handler(this::post);
+
+    templateEngine = ThymeleafTemplateEngine.create(vertx);
+
+    server.requestHandler(router)
+      .listen(8080, asyncResult -> {
+        if (asyncResult.succeeded()) {
+          LOGGER.info("HTTP server running on port 8080");
+          future.complete();
+        } else {
+          LOGGER.error("Could not start a HTTP server", asyncResult.cause());
+          future.fail(asyncResult.cause());
+        }
+      });
+
+    return future;
+  }
+
+  private void delete(RoutingContext context) {
+
+  }
+
+  private void index(RoutingContext context) {
+    templateEngine.render(context.data(), "templates/index.html", asyncResult -> {
+      if (asyncResult.succeeded()) {
+        context.response().end(asyncResult.result());
+      } else {
+        context.fail(asyncResult.cause());
+      }
+    });
+  }
+
+  private void post(RoutingContext context) {
+
   }
 
 }
--- exercise-53-todo-list/vertx/todo-list/src/test/java/com/example/todolist/TestMainVerticle.java
+++ exercise-53-todo-list/vertx/todo-list/src/test/java/com/example/todolist/TestMainVerticle.java
@@ -22,13 +22,13 @@ public class TestMainVerticle {
   }
 
   @Test
-  @DisplayName("Should start a Web Server on port 8888")
+  @DisplayName("Should start a Web Server on port 8080")
   @Timeout(value = 10, timeUnit = TimeUnit.SECONDS)
   void start_http_server(Vertx vertx, VertxTestContext testContext) throws Throwable {
-    vertx.createHttpClient().getNow(8888, "localhost", "/", response -> testContext.verify(() -> {
+    vertx.createHttpClient().getNow(8080, "localhost", "/todos", response -> testContext.verify(() -> {
       assertTrue(response.statusCode() == 200);
       response.handler(body -> {
-        assertTrue(body.toString().contains("Hello from Vert.x!"));
+        assertTrue(body.toString().contains("Todo List"));
         testContext.completeNow();
       });
     }));
