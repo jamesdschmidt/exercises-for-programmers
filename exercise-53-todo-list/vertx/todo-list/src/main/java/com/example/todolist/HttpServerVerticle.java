1c1d11587102fc8f83a578b820a5384e6d5b2706
James Schmidt <james.daniel.schmidt@gmail.com>
2019-05-08 07:32:43 -0600

    Add RedisVerticle

---
+++ exercise-53-todo-list/vertx/todo-list/src/main/java/com/example/todolist/HttpServerVerticle.java
@@ -0,0 +1,95 @@
+package com.example.todolist;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.eventbus.DeliveryOptions;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.templ.freemarker.FreeMarkerTemplateEngine;
+
+public class HttpServerVerticle extends AbstractVerticle {
+  private static final Logger LOGGER = LoggerFactory.getLogger(HttpServerVerticle.class);
+
+  public static final String CONFIG_HTTP_SERVER_PORT = "http.server.port";
+
+  private FreeMarkerTemplateEngine templateEngine;
+
+  @Override
+  public void start(Future<Void> startFuture) throws Exception {
+    templateEngine = FreeMarkerTemplateEngine.create(vertx);
+
+    var router = Router.router(vertx);
+    router.get("/").handler(this::index);
+    router.post().handler(BodyHandler.create());
+    router.post("/").handler(this::create);
+    router.post("/:id/delete").handler(this::delete);
+
+    int port = config().getInteger(CONFIG_HTTP_SERVER_PORT, 8080);
+
+    vertx
+      .createHttpServer()
+      .requestHandler(router)
+      .listen(port, asyncResult -> {
+        if (asyncResult.succeeded()) {
+          LOGGER.info("HTTP server running on port 8080");
+          startFuture.complete();
+        } else {
+          LOGGER.error("Could not start a HTTP server", asyncResult.cause());
+          startFuture.fail(asyncResult.cause());
+        }
+      });
+  }
+
+  private void delete(RoutingContext context) {
+    var id = context.request().getParam("id");
+    var options = new DeliveryOptions().addHeader("action", "delete");
+    vertx.eventBus().send(RedisVerticle.TODOS_ADDRESS, id, options, reply -> {
+      if (reply.succeeded()) {
+        redirect(context, "/");
+      } else {
+        context.fail(reply.cause());
+      }
+    });
+    redirect(context, "/");
+  }
+
+  private void index(RoutingContext context) {
+    var options = new DeliveryOptions().addHeader("action", "list");
+    vertx.eventBus().send(RedisVerticle.TODOS_ADDRESS, new JsonObject(), options, reply -> {
+      if (reply.succeeded()) {
+        context.put("todos", reply.result().body());
+        templateEngine.render(context.data(), "templates/index", asyncResult -> {
+          if (asyncResult.succeeded()) {
+            context.response().end(asyncResult.result());
+          } else {
+            context.fail(asyncResult.cause());
+          }
+        });
+      } else {
+        context.fail(reply.cause());
+      }
+    });
+  }
+
+  private void create(RoutingContext context) {
+    String todo = context.request().getParam("todo");
+    var options = new DeliveryOptions().addHeader("action", "create");
+    vertx.eventBus().send(RedisVerticle.TODOS_ADDRESS, todo, options, reply -> {
+      if (reply.succeeded()) {
+        redirect(context, "/");
+      } else {
+        context.fail(reply.cause());
+      }
+    });
+  }
+
+  private void redirect(RoutingContext context, String location) {
+    context.response().setStatusCode(303);
+    context.response().putHeader("Location", location);
+    context.response().end();
+  }
+}
---
+++ exercise-53-todo-list/vertx/todo-list/src/main/java/com/example/todolist/RedisVerticle.java
@@ -0,0 +1,124 @@
+package com.example.todolist;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.redis.client.Redis;
+import io.vertx.redis.client.RedisAPI;
+import io.vertx.redis.client.RedisOptions;
+
+import java.util.List;
+
+public class RedisVerticle extends AbstractVerticle {
+  private static final Logger LOGGER = LoggerFactory.getLogger(RedisVerticle.class);
+
+  public static final String TODOS_ADDRESS = "todos";
+
+  private static final int MAX_RECONNECT_RETRIES = 16;
+
+  private RedisOptions options = new RedisOptions();
+  private Redis client;
+
+  @Override
+  public void start(Future<Void> startFuture) throws Exception {
+    createRedisClient(onCreate -> {
+      if (onCreate.succeeded()) {
+        vertx.eventBus().consumer(TODOS_ADDRESS, this::onMessage);
+        startFuture.complete();
+      } else {
+        LOGGER.error("Database preparation error", onCreate.cause());
+        startFuture.fail(onCreate.cause());
+      }
+    });
+  }
+
+  public enum ErrorCodes {
+    NO_ACTION_SPECIFIED,
+    BAD_ACTION,
+    DB_ERROR
+  }
+
+  public void onMessage(Message<JsonObject> message) {
+    if (!message.headers().contains("action")) {
+      LOGGER.error("No action header specified for message with headers {} and body {}",
+        message.headers(), message.body().encodePrettily()); message.fail(ErrorCodes.NO_ACTION_SPECIFIED.ordinal(), "No action header specified");
+      return;
+    }
+    String action = message.headers().get("action");
+    switch (action) {
+      case "list":
+        list(message);
+        break;
+      case "create":
+        create(message);
+        break;
+      case "delete":
+        delete(message);
+        break;
+      default:
+        message.fail(ErrorCodes.BAD_ACTION.ordinal(), "Bad action: " + action);
+    }
+  }
+
+  /**
+   * Will create a redis client and setup a reconnect handler when there is
+   * an exception in the connection.
+   */
+  private void createRedisClient(Handler<AsyncResult<Redis>> handler) {
+    Redis.createClient(vertx, options)
+      .connect(onConnect -> {
+        if (onConnect.succeeded()) {
+          client = onConnect.result();
+          // make sure the client is reconnected on error
+          client.exceptionHandler(e -> {
+            // attempt to reconnect
+            attemptReconnect(0);
+          });
+        }
+        // allow further processing
+        handler.handle(onConnect);
+      });
+  }
+
+  /**
+   * Attempt to reconnect up to MAX_RECONNECT_RETRIES
+   */
+  private void attemptReconnect(int retry) {
+    if (retry > MAX_RECONNECT_RETRIES) {
+      // we should stop now, as there's nothing we can do.
+    } else {
+      // retry with backoff up to 1280ms
+      long backoff = (long) (Math.pow(2, MAX_RECONNECT_RETRIES - Math.max(MAX_RECONNECT_RETRIES - retry, 9)) * 10);
+
+      vertx.setTimer(backoff, timer -> createRedisClient(onReconnect -> {
+        if (onReconnect.failed()) {
+          attemptReconnect(retry + 1);
+        }
+      }));
+    }
+  }
+
+  private void list(Message<JsonObject> message) {
+  }
+
+  private void create(Message<JsonObject> message) {
+
+  }
+
+  private void delete(Message<JsonObject> message) {
+    var id = message.body().getString("id");
+
+    var redis = RedisAPI.api(client);
+    redis.del(List.of(id), res -> {
+      if (res.succeeded()) {
+        message.reply("success");
+      } else {
+      }
+    });
+  }
+}
