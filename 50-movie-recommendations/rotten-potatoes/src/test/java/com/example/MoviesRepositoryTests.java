c633bf9e0d9c1704f9539028bdc28d09819e8faa
James Schmidt <jschmidt@iofficecorp.com>
2022-01-20 11:31:47 -0700

    Update MoviesRepositoryTests.java

--- 50-movie-recommendations/rotten-potatoes/src/test/java/com/example/MoviesRepositoryTests.java
+++ 50-movie-recommendations/rotten-potatoes/src/test/java/com/example/MoviesRepositoryTests.java
@@ -1,35 +1,38 @@
 package com.example;
 
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertTrue;
-//
-//public class MoviesRepositoryTests {
-//
-//  private MoviesRepository repository = new MoviesRepository();
-//
-//  @Test
-//  public void testFindByTitleLikeShouldReturnEmptyListForNullSearch() {
-//    assertTrue(repository.findByTitleLike(null).isEmpty());
-//  }
-//
-//  @Test
-//  public void testFindByTitleLikeShouldReturnEmptyListForEmptySearch() {
-//    assertTrue(repository.findByTitleLike("").isEmpty());
-//  }
-//
-//  @Test
-//  public void testFindByTitleLikeShouldReturnEmptyListForNonMatchingSearch() {
-//    assertTrue(repository.findByTitleLike("").isEmpty());
-//  }
-//
-//  @Test
-//  public void testFindByTitleLikeShouldReturnOneResult() {
-//    assertTrue(repository.findByTitleLike("Guardians").size() == 1);
-//  }
-//
-//  @Test
-//  public void testFindByTitleLikeShouldReturnMoreResults() {
-//    assertTrue(repository.findByTitleLike("of the").size() > 1);
-//  }
-//}
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+public class MoviesRepositoryTests {
+
+  @InjectMocks
+  private MoviesRepository repository;
+
+  @Test
+  public void findByTitleLikeShouldReturnEmptyListForNullSearchTerm() {
+    assertThat(repository.findByTitleLike(null)).isEmpty();
+  }
+
+  @Test
+  public void findByTitleLikeShouldReturnEmptyListForEmptySearchTerm() {
+    assertThat(repository.findByTitleLike("")).isEmpty();
+  }
+
+  @Test
+  public void findByTitleLikeShouldReturnEmptyListForNonmatchingSearchTerm() {
+    assertThat(repository.findByTitleLike("non-matching")).isEmpty();
+  }
+
+  @Test
+  public void findByTitleLikeShouldSucceed() {
+    var rango = new Movie("1192628", "Rango", 2011, "PG", 105, 69,
+      "A chameleon (Johnny Depp) who has lived as a sheltered family pet finds himself in the grip of an identity crisis...");
+    var movies = repository.findByTitleLike("Rango");
+    assertThat(movies.contains(rango));
+  }
+}
